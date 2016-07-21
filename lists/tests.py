5a785bc28a5c34a1f691dea3f17e2d377a02ae54
Robert McBryde <rob@yavi.co>
2016-07-21 16:20:29 +0100

    Basic view now returns some html. Tests pass

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
@@ -8,3 +9,9 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEquals(found.func, home_page)
 
+	def test_homePageReturnsCorrectHtml(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
