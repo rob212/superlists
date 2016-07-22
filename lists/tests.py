be30c317b0421149098da8784a90f42a417774de
Robert McBryde <rob@yavi.co>
2016-07-22 18:28:02 +0100

    Addition of first variable in the home page template expanded the functional tests to allow for adding a second to-do list item changed the unit tests to strip the csfr token from the rendered home page template html to allow for testing consistently

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+import re
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.core.urlresolvers import resolve
@@ -10,8 +11,34 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEquals(found.func, home_page)
 
-    def test_homePageReturnsCorrectHtml(self):
+    def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+
+        # remove CSRF token to allow us to test
+        observed_html = self.remove_csrf_from_markup(response)
+
+        self.assertIn(expected_html, observed_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        expected_entry = 'A new list item'
+        request.POST['item_text'] = expected_entry
+
+        response = home_page(request)
+
+        self.assertIn(expected_entry, response.content.decode())
+        expected_html = render_to_string('home.html', {'new_item_text': expected_entry})
+
+        # remove CSRF token to allow us to test
+        observed_html = self.remove_csrf_from_markup(response)
+
+        self.assertEqual(expected_html, observed_html)
+
+    def remove_csrf_from_markup(self, response):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        observed_html = re.sub(csrf_regex, '', response.content.decode())
+        return observed_html
