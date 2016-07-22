ce759aefe4fc42819b1bca682ee670d95bcba869
Robert McBryde <rob@yavi.co>
2016-07-22 16:57:02 +0100

    Introduction of first template for home page addition of the lists app to the super list project refactoring of tests to to check the home page is returning the correct html via rendering the template to string and asserting it equals the response content of the view.

--- lists/tests.py
+++ lists/tests.py
@@ -1,17 +1,17 @@
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 
+
 class HomePageTest(TestCase):
+    def test_rootURL_resolvesToHomePageView(self):
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)
 
-	def test_rootURL_resolvesToHomePageView(self):
-		found = resolve('/')
-		self.assertEquals(found.func, home_page)
-
-	def test_homePageReturnsCorrectHtml(self):
-		request = HttpRequest()
-		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+    def test_homePageReturnsCorrectHtml(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
