3c31da90c9a7a6f0377c82e2dbd67965182e1714
Robert McBryde <rob@yavi.co>
2016-07-24 22:01:26 +0100

    Added for loop of items in home_page template and loop counter to render things to do list Updated home_page View in order to get all the items from the db and pass them the to home_page template in a dictionary Added a unit test to ensure all the items from the db are displayed on the home_page response and refactored indentation spacing of the unit tests.

--- lists/tests.py
+++ lists/tests.py
@@ -8,65 +8,75 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
-	def setUp(self):
-		self.EXPECTED_ITEM_TEXT = 'A new list item'
-		self.request = HttpRequest()
-		self.request.method = 'POST'
-		self.request.POST['item_text'] = self.EXPECTED_ITEM_TEXT
+    def setUp(self):
+        self.EXPECTED_ITEM_TEXT = 'A new list item'
+        self.request = HttpRequest()
+        self.request.method = 'POST'
+        self.request.POST['item_text'] = self.EXPECTED_ITEM_TEXT
 
-	def test_rootURL_resolvesToHomePageView(self):
-		found = resolve('/')
-		self.assertEquals(found.func, home_page)
+    def test_rootURL_resolvesToHomePageView(self):
+        found = resolve('/')
+        self.assertEquals(found.func, home_page)
 
-	def test_home_page_only_saves_items_when_they_exist_in_request(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(), 0)
+    def test_home_page_only_saves_items_when_they_exist_in_request(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
 
-		expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html')
 
-		# remove CSRF token to allow us to test
-		observed_html = self.remove_csrf_from_markup(response)
+        # remove CSRF token to allow us to test
+        observed_html = self.remove_csrf_from_markup(response)
 
-		self.assertIn(expected_html, observed_html)
+        self.assertIn(expected_html, observed_html)
 
-	def test_home_page_can_save_a_POST_request(self):
-		home_page(self.request)
+    def test_home_page_can_save_a_POST_request(self):
+        home_page(self.request)
 
-		self.assertEqual(Item.objects.count(), 1)
-		retrieved_item = Item.objects.first()
-		self.assertEqual(retrieved_item.text, self.EXPECTED_ITEM_TEXT)
+        self.assertEqual(Item.objects.count(), 1)
+        retrieved_item = Item.objects.first()
+        self.assertEqual(retrieved_item.text, self.EXPECTED_ITEM_TEXT)
 
-	def test_home_page_redirects_after_POST(self):
-		response = home_page(self.request)
+    def test_home_page_redirects_after_POST(self):
+        response = home_page(self.request)
 
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
-	def remove_csrf_from_markup(self, response):
-		csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
-		observed_html = re.sub(csrf_regex, '', response.content.decode())
-		return observed_html
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text="Itemy 1")
+        Item.objects.create(text="Itemy 2")
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('Itemy 1', response.content.decode())
+        self.assertIn('Itemy 2', response.content.decode())
+
+    def remove_csrf_from_markup(self, response):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        observed_html = re.sub(csrf_regex, '', response.content.decode())
+        return observed_html
 
 
 class ItemModelTest(TestCase):
-	def test_saving_and_retrieving_items(self):
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-		second_item = Item()
-		second_item.text = 'The second item'
-		second_item.save()
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEquals(saved_items.count(), 2)
+        saved_items = Item.objects.all()
+        self.assertEquals(saved_items.count(), 2)
 
-		retrieved_first_item = saved_items[0]
-		retrieved_second_item = saved_items[1]
-		self.assertEqual(retrieved_first_item.text, first_item.text)
-		self.assertEqual(retrieved_second_item.text, second_item.text)
+        retrieved_first_item = saved_items[0]
+        retrieved_second_item = saved_items[1]
+        self.assertEqual(retrieved_first_item.text, first_item.text)
+        self.assertEqual(retrieved_second_item.text, second_item.text)
