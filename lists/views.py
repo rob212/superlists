26aa03ef0d231222e458e463d8b230576ca720d6
Robert McBryde <rob@yavi.co>
2016-07-27 00:25:33 +0100

    New List View and template to display a users list.
    reformatted test to use the django TestCase class that allows for the use of the client to get url of the view and easier asserts.

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,12 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/lists/the_only_list_in_the_world')
+	return render(request, 'home.html')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+
+def view_list(request):
+	items = Item.objects.all()
+	return render(request, 'list.html', {'items': items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,4 +20,5 @@ from lists import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the_only_list_in_the_world$', views.view_list, name='view_list'),
 ]
